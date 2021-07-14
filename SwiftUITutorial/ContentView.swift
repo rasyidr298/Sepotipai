cc9349d0d2ca603d31742794df83cde4991d662e
Tafani Rabbani <ratafani@gmail.com>
2021-07-14 10:24:00 +0700

    add State and Binding

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -11,37 +11,57 @@ struct Song: Identifiable {
     var id = UUID()
     var singer: String
     var title: String
+    
 }
 
 struct ContentView: View {
     var playlist = [Song(singer: "U2", title: "Elevation"), Song(singer: "Ciara", title: "Level up")]
     
+    @State private var titleSongPlayed : String = ""
+    @State private var isPlayingSomething : Bool = false
+    @State private var userName : String = ""
+    
     var body: some View {
         NavigationView(){
-        VStack{
-            //Text("Playlist").font(.largeTitle)
-            HStack{
-                Button(action: {
-                    print("play Playlist")
-                }, label: {
-                    Image(systemName: "play.circle.fill").font(.system(size: 56)).foregroundColor(.green)
-                })
-                Button(action: {
-                    print("play Playlist")
-                }, label: {
-                    Text("❤️")
-                })
-            }.frame(width: 350, height: 100, alignment: .leading)
-            List(playlist){ i in
-                    Text(i.singer + " - " + i.title)
-            }
-        }.navigationBarTitle(Text("PlayList")).foregroundColor(.gray)
+            VStack{
+                HStack{
+                    Button(action: {
+                        print("play Playlist")
+                        isPlayingSomething.toggle()
+                    }, label: {
+                        if isPlayingSomething{
+                            Image(systemName: "pause.circle.fill").font(.system(size: 56)).foregroundColor(.blue)
+                        }else{
+                            Image(systemName: "play.circle.fill").font(.system(size: 56)).foregroundColor(.green)
+                        }
+                        
+                    })
+                    Text(titleSongPlayed)
+                }.frame(width: 350, height: 100, alignment: .leading)
+                TextField("Siapa namamu?", text: $userName).padding()
+                List(playlist){ i in
+                    SongCellCustom(song: i, titleSongPlayed: $titleSongPlayed, isPlayingSomething: $isPlayingSomething)
+                }
+            }.navigationBarTitle(Text(userName)).foregroundColor(.gray)
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct SongCellCustom : View {
+    let song : Song
+    
+    @Binding var titleSongPlayed : String
+    @Binding var isPlayingSomething : Bool
+    var body: some View{
+        Button {
+            titleSongPlayed = song.singer + " - " + song.title
+            isPlayingSomething = true
+        } label: {
+            HStack{
+                Text(song.singer + " - " + song.title)
+                Spacer()
+                Image(systemName: "play.circle.fill").font(.system(size: 30)).foregroundColor(.green)
+            }
+        }
     }
 }
